8769d0f08b53ee550ee797ab6bd8d09b9ae7b661
Mohammed Ibrahim Farooqui <fmohammedibr@vmware.com>
2022-09-30 19:16:24 +0530

    Adding more tests

--- generic-tools/src/test/java/org/tools/desktop/generic/grep/GrepMainTest.java
+++ generic-tools/src/test/java/org/tools/desktop/generic/grep/GrepMainTest.java
@@ -11,6 +11,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 import java.util.UUID;
 
 public class GrepMainTest {
@@ -30,7 +31,7 @@ public class GrepMainTest {
   }
 
   @Test
-  public void test() throws Exception {
+  public void canApplyGenericFilters() throws Exception {
     Path temp = getTempDirectoryInstance();
     saveFile(Paths.get(temp.toString(), "file1.txt"), "one two three");
     saveFile(Paths.get(temp.toString(), "file2.txt"), "one two unexpected token three"); //rejected for line must not contain "unexpected token"
@@ -40,31 +41,69 @@ public class GrepMainTest {
     saveFile(Paths.get(temp.toString(), "file_name_must_not_contain.txt"), "one two three"); //rejected for filepath must not contain 'must_not_contain'
 
     Path resultsFile = Paths.get(temp.toString(), "results.log");
-    GrepMain.startProcessing(
-        temp.toString(),
-        null,
-        resultsFile.toString(),
-        Collections.singletonList("txt"),
-        Collections.emptyList(),
-        Collections.singletonList("must_not_contain"),
-        Arrays.asList("one two", "three"),
-        Arrays.asList("unexpected token", "unexpected_token", "gz", "ux"),
-        false
-    );
+    String srcDir = temp.toString();
+    String outputDir = null;
+    String outputFile = resultsFile.toString();
+    List<String> fileExtension = Collections.singletonList("txt");
+    List<String> fileNameMustContain = Collections.emptyList();
+    List<String> filePathMustNotContain = Collections.singletonList("must_not_contain");
+    List<String> lineMustContain = Arrays.asList("one two", "three");
+    List<String> lineMustNotContain = Arrays.asList("unexpected token", "unexpected_token", "gz", "ux");
 
+    GrepMain.startProcessing(srcDir, outputDir, outputFile, fileExtension, fileNameMustContain, filePathMustNotContain, lineMustContain, lineMustNotContain, false);
 
     String contents = FileUtils.readFileToString(resultsFile.toFile(), StandardCharsets.UTF_8.name());
     Assert.assertEquals(contents, "one two three" + GrepMain.NEW_LINE_CHAR);
-    /**
-     * String srcDir,
-     *       String outputDir,
-     *       String outputFile,
-     *       List<String> fileExtension,
-     *       List<String> fileNameMustContain,
-     *       List<String> filePathMustNotContain,
-     *       List<String> lineMustContain,
-     *       List<String> lineMustNotContain,
-     *       boolean addTimestampToResultantFileName
-     */
+
+  }
+
+  @Test
+  public void canIgnoreFilePaths() throws Exception {
+    Path temp = getTempDirectoryInstance();
+    String sampleContent = "one two three";
+    saveFile(Paths.get(temp.toString(), "file1.txt"), sampleContent);
+
+    Path newSubDir = Paths.get(temp.toString(), "dir_to_ignore", "sub_folder");
+    newSubDir.toFile().mkdirs();
+    saveFile(Paths.get(newSubDir.toString(), "file2.txt"), sampleContent);
+
+    Path resultsFile = Paths.get(temp.toString(), "results.log");
+    String srcDir = temp.toString();
+    String outputDir = null;
+    String outputFile = resultsFile.toString();
+    List<String> fileExtension = Collections.singletonList("txt");
+    List<String> fileNameMustContain = Collections.emptyList();
+    List<String> filePathMustNotContain = Collections.singletonList("dir_to_ignore" + System.getProperty("file.separator") + "sub_folder");
+    List<String> lineMustContain = Collections.singletonList(sampleContent);
+    List<String> lineMustNotContain = Collections.emptyList();
+
+    GrepMain.startProcessing(srcDir, outputDir, outputFile, fileExtension, fileNameMustContain, filePathMustNotContain, lineMustContain, lineMustNotContain, false);
+
+    String contents = FileUtils.readFileToString(resultsFile.toFile(), StandardCharsets.UTF_8.name());
+    Assert.assertEquals(contents, sampleContent + GrepMain.NEW_LINE_CHAR);
+  }
+
+  @Test
+  public void canGivePrecedenceToLineMustNotContainEvenThoughLineMatchesPattern() throws Exception {
+    Path temp = getTempDirectoryInstance();
+    String sampleContent = "one two three";
+    String unexpectedContent = "unexpected content";
+    saveFile(Paths.get(temp.toString(), "file1.txt"), sampleContent);
+    saveFile(Paths.get(temp.toString(), "file2.txt"),  sampleContent + " " + unexpectedContent);
+
+    Path resultsFile = Paths.get(temp.toString(), "results.log");
+    String srcDir = temp.toString();
+    String outputDir = null;
+    String outputFile = resultsFile.toString();
+    List<String> fileExtension = Collections.emptyList();
+    List<String> fileNameMustContain = Collections.emptyList();
+    List<String> filePathMustNotContain = Collections.singletonList("dir_to_ignore" + System.getProperty("file.separator") + "sub_folder");
+    List<String> lineMustContain = Collections.singletonList(sampleContent);
+    List<String> lineMustNotContain = Collections.singletonList(unexpectedContent);
+
+    GrepMain.startProcessing(srcDir, outputDir, outputFile, fileExtension, fileNameMustContain, filePathMustNotContain, lineMustContain, lineMustNotContain, false);
+
+    String contents = FileUtils.readFileToString(resultsFile.toFile(), StandardCharsets.UTF_8.name());
+    Assert.assertEquals(contents, sampleContent + GrepMain.NEW_LINE_CHAR);
   }
 }
